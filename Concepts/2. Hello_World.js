a1687e68da42e5191cd806d18be6b1bb4f766403
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-08 11:08:14 +0530

    Create 2. Hello_World.js

---
+++ Concepts/2. Hello_World.js
@@ -0,0 +1,18 @@
+1. to make next js project 
+
+npx create-next-app hello-world
+
+
+in package.json it has 3 dependecies by default.
+
+1. next  - it a framework built on top of react which contains all the pkgs and code required to build feature rich app
+2. react - to build UI
+3. react-dom - to mount react app
+
+
+project flow
+
+1. npm run dev > it goes to _app.js file where it receives component as its arg > that component arg is coming from index.js
+
+simple words - npm run dev -> _app.js -> index.js
+
