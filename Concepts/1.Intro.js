4979c5e03ccc51abff4bb7667716153e912ef1ea
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-08 10:48:46 +0530

    Update 1.Intro.js

--- Concepts/1.Intro.js
+++ Concepts/1.Intro.js
@@ -2,9 +2,29 @@
 1. What is Next JS?
 Its a React framework for production.
 
-React:-
+React JS:-
 When we build any application with only React then its not quite possible to build a full feature rich application ready to be deployed for production.
 Its just a JS library to build user interfaces.
 It is responsible for only view layer of an application.
-to build routing n all we need to add extra packages like react-router.
+to build routing & other features like authentication, styling we need to add extra packages like react-router.
+
+
+Next JS:-
+It uses React for building user interfaces.
+Its a framework.
+Comes with lot more features like routing, styling, authentication etc. that enables to build full feature rich application.
+There is no need to install additional pkgs, Next JS provides you everything out of the box.
+
+
+
+Why Next JS?
+  simplifies the process of building react applications for production.
+  some of the features:-
+  1. File based Routing
+  2. Pre - rendering  -  creates html in advance instead of converting JSX to html at client-side
+  3. API routes
+  4. support for CSS modules
+  5. Authentication  - supports multiple authentication patterns
+  6. Dev & Prod build system
+  
 
