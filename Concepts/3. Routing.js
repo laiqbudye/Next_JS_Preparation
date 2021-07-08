89e5933aa4f8006d3b61a24697c457619312b1a1
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-08 11:21:31 +0530

    Create 3. Routing.js

---
+++ Concepts/3. Routing.js
@@ -0,0 +1,22 @@
+Next Js has implemented their own routing mechanism.
+
+1. File-system based routing
+    When a file is added to the pages folder in a project, it autimatically becomes available as route.
+    
+    i) Routing with pages
+     
+    - localhost:3000/
+    - when we add index.js file inside pages folder it acts as a root.
+    
+    - index.js
+      function Home(){
+        return <h1> Home page </h1>
+      }
+
+    so when user hits localhost:3000/ then it will go to pages folder & look for index.js & render that page.
+    so the output in this case will be "Home page"
+
+
+
+    - localhost:3000/about
+    - to show this route we need to add about.js file under pages folder
