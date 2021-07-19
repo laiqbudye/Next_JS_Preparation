d696f0f5d06b75d9c892d173c901a3dc3f42804e
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-19 22:33:52 +0530

    Update 9. getStaticPaths_Fallback.js

--- Concepts/9. getStaticPaths_Fallback.js
+++ Concepts/9. getStaticPaths_Fallback.js
@@ -26,12 +26,19 @@ Fallback
   
   - if fallback is set to true, then any path which is not returned by getStaticPaths will not show 404 page. instead next.js will serve a fallback version of the page
     on the first request to such a path.
-      e.g if our getStaticPath returning path for posts 1, 2, 3 & user entered 4 then it will fallback page
+      e.g if our getStaticPath returning path for posts 1, 2, 3 & user entered 4 then it will show fallback page
       
-  - in the background, next.js will statically generate the requested path HTML & JSON. this includes running getStaticProps.
+  - then in the background, next.js will statically generate the requested path HTML & JSON. this includes running getStaticProps.
   
   - when thats done the browser receives the JSON which will be used to automatically render the page. from user point of view, the page will be swapped from 
     fallback page to the full page.
     
     
+    
+    in my words,
+      when fallback is set to true, then only paths returned from getStaticPaths are pre-rendered at build time.
+      when user enters a path that is not pre rendered / not returned by getstaticpaths then it will show a fallback screen & at the same time using getStaticProps it will fetch data for that
+      entered URL in the bg & once ready it will show that to user.
+      
+      so when user hits posts/6 for the first time it will show fallback UI until data gets fetched in the bg & when user hits same URL second time it will show actual content
       
