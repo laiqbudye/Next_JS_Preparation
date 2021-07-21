25e60ceee51dbdefe236750316a9d28a3e5e1baa
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-07-22 00:06:37 +0530

    Update 11. SSR.js

--- Concepts/11. SSR.js
+++ Concepts/11. SSR.js
@@ -1,1 +1,11 @@
 
+SSR - Server Side Rendering
+  next js allows to pre-render a page not at build time but at request time.
+  HTML is generated for every incoming request.
+  user requests for a page, next js fetches data for that page from an API, then HTML is generated for fetched data & it is sent back to the browser.
+  
+  When to use?
+    when you need to fetch data per request & also when you need to fetch personalized data keeping in mind SEO.
+  
+  
+ 
