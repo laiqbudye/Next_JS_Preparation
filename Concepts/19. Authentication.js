1c2768dd4f81482a1a5892c56fa3201244fdb68b
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-19 18:12:48 +0530

    Create 19. Authentication.js

---
+++ Concepts/19. Authentication.js
@@ -0,0 +1,17 @@
+when it comes to authentication, there are two things comes in picture.
+
+1. Authentication - whether the user is identified or not
+2. Authorization - whether the user has permissions or not
+
+
+to deal with authentication in nextJS there is very popular pkg which is:- next-auth
+
+  npm i next-auth
+
+
+
+
+
+
+
+
