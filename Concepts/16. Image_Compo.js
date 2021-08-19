7693a3f601773d88a3cfee838c2a21c72f5090d2
Laiq Budye <40020518+laiqbudye@users.noreply.github.com>
2021-08-19 13:04:51 +0530

    Update 16. Image_Compo.js

--- Concepts/16. Image_Compo.js
+++ Concepts/16. Image_Compo.js
@@ -33,8 +33,34 @@ Problems with above approach,
 
 ------------------------------------------------------------------------------------------------------------------------------
 
-same example with 
+same example with nextJS Image tag -
 
+import Image from 'next/Image';
+
+function PetsPage() {
+  return (
+    <div>
+      {
+        ['1','2','3','4','5'].map( path => {        // consider '1','2','3' as image names
+            return (
+              <div key={path}>
+                <Image src={`/${path}.jpg`} alt='pet' width='280' height='420'/>
+              </div>
+            )
+        })
+      }
+    </div>
+  )
+}
+
+export default PetsPage
+
+
+Benefits with above approach,
+  - it will reduce the image size & then dwnload. for 2MB file it will reduce than to 30kb & then dwnload
+  - it supports lazy loading. (does not fetch all images at initial load, only fetches images which are visible to user on initial load & then based on scrolling it will make new requests)
+  - even if we specify jpg image, it converts jpg to webp which is more optimized & faster
+  - with placeholder= {blur} prop (this prop works with only dynamic images), it will render blur version of the image till the time actual image gets downloaded
 
 
 
